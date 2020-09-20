6b1c7045ec493a87d1d41419c649a16a9012fce3
Eloise Zeng <eloise.zeng@gmail.com>
2020-09-20 12:36:21 -0400

    css

--- src/components/pages/SettingsHeader.js
+++ src/components/pages/SettingsHeader.js
@@ -4,7 +4,7 @@ export default function SettingsHeader() {
     return (
         <div>
             <h1>Settings</h1>
-            <details><summary><h3>Instructions</h3></summary>
+            <details><summary><h2>Instructions</h2></summary>
             <p>Recommended: Install the latest version of zoom. Quit zoom and reopen it before setting pixels.</p>
             <p>Press the dropdown arrows to see images of the buttons.</p>
             <p>Press Set, then click a button on your computer. 
